7b593d17105bae57060a98d09c2ee5ee61ee28a1
peterwade153 <peterwade153@gmail.com>
2018-02-20 15:38:14 +0300

    add class for business reviews

---
+++ classes/review.py
@@ -0,0 +1,11 @@
+""" reviews class"""
+
+class Review(object):
+	"""docstring for Review"""
+	def __init__(self, reviews):
+		self.reviews = []
+		
+	def __repr__():
+		return 'Reviews: ' + ','.join(review for review in self.reviews)
+
+		
