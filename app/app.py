f535d825371ae83cbdf0a003d858bbbd10449083
peterwade153 <peterwade153@gmail.com>
2018-02-21 15:35:44 +0300

    modifying signup n login features

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ def register():
 
 	data = request.get_json()
 	if not data['username'] and not data['password']:
-		return make_response(jsonify({'message':'please fill in username and password'})),403
+		return make_response(jsonify({'message': 'please fill in username and password'})),403
 
 	username = data['username']
 	password = data['password']
@@ -28,11 +28,28 @@ def register():
 	''' checking if user is not already registered '''
 	if username not in users.keys():
 		users['username'] = password
-		return make_response(jsonify({'message':'registered successfully'})),201
+		return make_response(jsonify({'message': 'registered successfully'})),201
 
-	return make_response(jsonify({'message':'user already registered'})),202
+	return make_response(jsonify({'message': 'user already registered'})),202
 
 
+@app.route('/api/v1/auth/login', methods = ['POST'])
+def login():
+	""" route enables a user to login """
+
+	data = request.get_json()
+	if not data['username'] and not data['password']:
+		return make_response(jsonify({'message': 'please fill in username and password'}))
+
+	username = data['username']
+	password = data['password']
+
+	''' checking user is already registered '''
+	if username not in users.keys() and password not in users.values():
+		return make_response(jsonify({'message': 'please register to login'}))
+
+	session['logged_in'] = True
+	return make_response(jsonify({'message': 'logged in successfully!'}))
 
 
 if __name__ == '__main__':
