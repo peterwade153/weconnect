336bf0856034c2640074b4cc240cde884737bb3c
peterwade153 <peterwade153@gmail.com>
2018-02-21 09:25:29 +0300

    adding testcases for login and registration

--- tests/test_api1.py
+++ tests/test_api1.py
@@ -1,17 +1,43 @@
+import json
 import unittest
+from app import app
 
 class BusinessTestCase(unittest.TestCase):
 
 	def SetUp(self):
 		""" this is run before every test"""
-		pass
+		self.app = app.test_client()
+		app.testing = True
 
 	def TearDown(self):
 		""" this runs after every test"""
 		pass
 
+	def test_user_registration(self):
+		""" tests user is registered """
+
+		user_data = {'username': 'test', 'password':'test123'}
+		result = self.app.post('/api/v1/auth/register', data = json.dumps(user_data))
+		self.assertIn('registered successfully', str(result.data))
+		self.assertEqual(result.status_code, 201)
+
+
+	def test_user_login(self):
+		""" tests a user can login """
+
+		user_data = {'username': 'test', 'password':'test123'}
+		result2 = self.app.post('/api/v1/auth/login', data = json.dumps(user_data))
+		self.assertIn('logged in successfully!', str(result2.data))
+		self.assertEqual(result2.status_code, 200)
+
+
+
 	def test_business_registered_successfully(self):
 		""" tests a business can be created successfully """
+
+		business_data = {'id': 1, 'business_name':'demo'}
+
+
 		pass
 
 	def test_registering_already_existing_business(self):
