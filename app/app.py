171c6c8aa51128d6467844995d5ee149f37969e8
peterwade153 <peterwade153@gmail.com>
2018-02-21 22:13:46 +0300

    added get all businesses feature

--- app/app.py
+++ app/app.py
@@ -47,16 +47,78 @@ def login():
 	''' checking user is already registered '''
 	if username in users.keys() and password in users.values():
 		session['logged_in'] = True
-		return make_response(jsonify({'message': 'logged in successfully!'})),200
+		return make_response(jsonify({'message': 'logged in successfully!'})), 200
 
 	return make_response(jsonify({'message': 'please register to login'})), 403
+
 
 @app.route('/api/v1/auth/logout', methods = ['POST'])
 def logout():
 	""" route for logging out alogged in user """
 
 	session.pop('logged_in', None)
-	return jsonify({'message': 'successfully logged out'})
+	return jsonify({'message': 'successfully logged out'}), 200
+
+
+@app.route('/api/v1/auth/reset-password', methods = ['POST'])
+def reset_password():
+	""" route enables user reset their password """
+
+	data = request.get_json()
+	if data['username'] and data['new_password']:
+		''' check if user is already registered here'''
+		if data['username'] in users.keys():
+			users['username'] = data['new_password']
+			return jsonify({'message': 'password reset successfully!'}), 200
+
+		return jsonify({'message': 'user not registered here!'}), 404
+
+	return jsonify({'message': 'please fill in username and password'}), 403
+
+
+@app.route('/api/v1/businesses', methods = ['POST'])
+def register_business():
+	""" route enables user registera business"""
+
+	data = request.get_json()
+	if data['id'] and data['business_name']:
+		user.register_business(data['id'], data['business_name'])
+		return jsonify({'message': 'Business registered successfully'}), 201
+
+	return ({'message': 'please fill in business id and business_name'}), 403
+
+
+
+@app.route('/api/v1/businesses/<business_id>', methods = ['PUT'])
+def update_business(business_id):
+	""" route allows a user update a business """
+
+	data = request.get_json()
+	if data['new_name']:
+		user.update_registered_business(business_id, data['new_name'])
+		return jsonify({'message': 'Business updated successfully!'}), 200
+
+	return jsonify({'message': 'please fill in id and new_name'}), 403
+
+
+@app.route('/api/v1/businesses/<business_id>', methods = ['DELETE'])
+def delete_business(business_id):
+	""" route should enable a user delete a registered business """
+
+	user.delete_registered_business(business_id)
+	return jsonify({'message': 'Business deleted successfully'}), 200
+
+
+@app.route('/api/v1/businesses', methods = ['GET'])
+def view_businesses():
+	""" route returns all businesses """
+
+	my_list = user.view_registered_businesses()
+	if not my_list:
+		return jsonify({'message': 'no business registered'}), 404
+
+	return jsonify({'businesses': my_list}), 200
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
