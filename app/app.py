830d085b3a9c45a88924cf0ba5abc687d97f91a4
peterwade153 <peterwade153@gmail.com>
2018-03-01 20:04:01 +0300

    fixed pep8 issues

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('..')
+import re
 import json
 from flask import Flask, request, session, jsonify, make_response, render_template
 from classes.user import User
@@ -27,40 +28,50 @@ def register():
 	""" route enables a user to register """
 
 	data = request.get_json()
-	if not data['username'] and not data['password']:
-		return make_response(jsonify({'message': 'please fill in username and password'})), 403
-	#stripping off any leading and tailing whitespaces both password and username
+
 	username = data['username']
-	username.strip()  
+	name_match = re.match('([A-Za-z0-9])', username)
 	password = data['password']
-	password.strip()   
+	password_match = re.match('([A-Za-z0-9]{4,})', password)
 
-	''' checking if user is not already registered '''
-	if username not in users.keys():
-		users[username] = password
-		return make_response(jsonify({'message': 'registered successfully'})), 201
-	return make_response(jsonify({'message': 'user already exists, please choose another username'})), 202
+	if name_match and password_match:
+		''' checking if user is not already registered '''
+		if username not in users.keys():
+			users[username] = password
+			return jsonify({'success': True,
+							'message': 'Registered successfully',
+				             'data': {'Username': username}
+				             }), 201
 
+		return jsonify({'success': False,
+			            'message': 'User already registered!'}), 202
+	else:
+		return jsonify({'success': False,
+			            'message': 'Your password should be atleast 4 characters, no special characters'}),403
 
 @app.route('/api/v1/auth/login', methods = ['POST'])
 def login():
 	""" route enables a user to login """
 
 	data = request.get_json()
-	if not data['username'] and not data['password']:
-		return make_response(jsonify({'message': 'please fill in username and password'})), 403
 
 	username = data['username']
-	username.strip()
+	name_match = re.match('([A-Za-z0-9])', username)
 	password = data['password']
-	password.strip()
+	password_match = re.match('([A-Za-z0-9]{4,})', password)
 
-	''' checking user is already registered '''
-	if username in users.keys() and password in users.values():
-		session['logged_in'] = True
-		return make_response(jsonify({'message': 'logged in successfully!'})), 200
+	if name_match and password_match:
 
-	return make_response(jsonify({'message': 'username and password dont match'})), 404
+		#if check_password_hash(users[username], password):
+		if ( username, password ) in users.items():
+			''' checking user is already registered '''
+			session['logged_in'] = True
+			return jsonify({'success': True,
+				            'message': 'Logged in successfully!'}), 200
+		return jsonify({'message': 'Username and password dont match'}), 401
+	else:
+		return jsonify({'success': False, 
+			            'message':'Username and password should atleast be 4 characters or digits'}),400
 
 
 @app.route('/api/v1/auth/logout', methods = ['POST'])
@@ -68,7 +79,8 @@ def logout():
 	""" route for logging out alogged in user """
 
 	session.pop('logged_in', None)
-	return jsonify({'message': 'successfully logged out'}), 200
+	return jsonify({'success': True,
+		            'message': 'Logged out'}), 200
 
 
 @app.route('/api/v1/auth/reset-password', methods = ['POST'])
@@ -79,20 +91,23 @@ def reset_password():
 	
 	''' check if user is already registered here'''
 	username = data['username']
-	username.strip()
+	name_match = re.match('([A-Za-z0-9]{4,})', username)
 	password = data['password']
-	password.strip()
+	password_match = re.match('([A-Za-z0-9]{4,})', password)
 	new_password = data['new_password']
-	new_password.strip()
-    
-    #check if passed username and password mat
-	if ( username, password ) in users.items():
-		users['username'] = new_password
-		return jsonify({'message': 'password reset successfully!'}), 200
+	new_password_match = re.match('([A-Za-z0-9]{4,})', new_password)
 
-	return jsonify({'message': 'user not registered here!'}), 404
+	if name_match and password_match and new_password_match:
+		if ( username, password ) in users.items():
+			users['username'] = password
+			return jsonify({'success':True,
+				            'message': 'password reset successfully!'}), 200
 
-	#return jsonify({'message': 'please fill in username and password'}), 403
+		return jsonify({'success':True,
+			            'message': 'user not registered here!'}), 404
+
+	return jsonify({'success': False,
+		            'message':'username and password atleast 4 characters or digits'}), 400
 
 
 @app.route('/api/v1/businesses', methods = ['POST', 'GET'])
@@ -103,21 +118,27 @@ def business():
 		""" registering new business"""
 
 		data = request.get_json()
-		if data['business_name']:
-			business = user.register_business(data['business_name'])
-			return jsonify({'Business registered successfully': business }), 201
+		name = data['business_name']
+		business_match = re.match('([A-Za-z0-9])', name)
+		if business_match:
+			business = user.register_business(name)
+			return jsonify({'success':True,
+				            'Business registered successfully': business }), 201
 
-		return jsonify({'message': 'please fill in business_name'}), 400
-
+		return jsonify({'success':False,
+			            'message': 'Business_name should be characters or digits'}), 400
+ 
 	else:
 		# GET
 		""" return registered businesses """
 
 		my_businesses = user.view_registered_businesses()
 		if not my_businesses:
-			return jsonify({'message': 'no business registered'}), 404
+			return jsonify({'success': False,
+				            'message': 'no business registered'}), 404
 
-		return jsonify({'businesses': my_businesses}), 200
+		return jsonify({'success': True,
+			            'businesses': my_businesses}), 200
 
 
 @app.route('/api/v1/businesses/<id>', methods = ['PUT', 'GET', 'DELETE'])
@@ -128,14 +149,19 @@ def update_get_delete_business(id):
 		""" Update  a registered business """
 
 		data = request.get_json()
-		if data['new_name']:
+		name = data['new_name']
+		name_match = re.match('([A-Za-z0-9])', name)
+		if name_match:
 			update = user.update_registered_business(int(id), data['new_name'])
 			if update:
-				return jsonify({'Business updated to': update}), 200
+				return jsonify({'success':True,
+					            'Business updated to': update}), 200
 
-			return jsonify({'message': 'Business not registered here!'}), 404
+			return jsonify({'Success':False,
+				            'message': 'Business not registered here!'}), 404
 
-		return jsonify({'message': 'please fill new_name'}), 400
+		return jsonify({'success': False,
+		                'message': 'New name should be characters or digits'}), 400
 
 
 	elif request.method == "DELETE":
@@ -143,18 +169,22 @@ def update_get_delete_business(id):
 
 		business = user.delete_registered_business(int(id))
 		if not business:
-			return jsonify({'message': 'business not registered here'}), 404
+			return jsonify({'success': True,
+				            'message': 'business not registered here'}), 404
 
-		return jsonify({'message': 'Business deleted successfully'}), 200
+		return jsonify({'success': False,
+			            'message': 'Business deleted successfully'}), 200
 
 	else:
 		""" enable user view a registered business """
 
 		business = user.view_a_business(int(id))
 		if not business:
-			return jsonify({'message': 'Business not registered here'}), 404
+			return jsonify({'success': False,
+				            'message': 'Business not registered here'}), 404
 
-		return jsonify({'Business': business}),200
+		return jsonify({'success': True,
+		                'Business': business}),200
 
 
 
@@ -165,21 +195,26 @@ def review_and_view_business_reviews(business_id):
 	if request.method == 'POST':
 		""" method to add review"""
 		data = request.get_json()
-		if data['review']:
-			my_review = user.add_review(business_id, data['review'])
+		reviews = data['review']
+		reviews_match = re.match('([A-Za-z0-9])', reviews)
+		if reviews_match:
+			my_review = user.add_review(int(business_id), data['review'])
 			if my_review:
 				return jsonify({'message': 'Review added successfully!'}), 201
 
 			return jsonify({'message':'business not registered here'}), 404
 
-		return jsonify({'message': 'please enter review'}), 403
+		return jsonify({'message': 'Review should have only charaters and digits'}), 403
 
+	#GET
 	else:
-		business = user.view_business_reviews(business_id)
+		business = user.view_business_reviews(int(business_id))
 		if business:
-			return jsonify({'reviews': business}), 200
+			return jsonify({'success': True,
+				            'reviews': business}), 200
 
-		return jsonify({'message':'no reviews registered yet'}), 404
+		return jsonify({'success': False,
+			            'message':'no reviews registered yet'}), 404
 
 
 if __name__ == '__main__':
