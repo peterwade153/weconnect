6254445cdb02e5d746e73b75063fc131b43a2d1f
peterwade153 <peterwade153@gmail.com>
2018-03-21 12:02:27 +0300

    Fixed login issues

--- api/v2/app/views.py
+++ api/v2/app/views.py
@@ -1,6 +1,7 @@
 import os
 from app import app
 from app.models import db
+from app.auth.views import auth
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -9,3 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 db.init_app(app) #connecting sqlalchemy object to the app
 
+
+
+
+@app.route('/api/v2/auth/logout' , methods = ['POST'])
+def logout_user(self):
+	""" route logs out a user """
