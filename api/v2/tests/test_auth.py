feb935eff7687a0140e0f1e8f074fd9fa27a06e4
peterwade153 <peterwade153@gmail.com>
2018-03-23 11:49:08 +0300

    add testcase for reset-password

--- api/v2/tests/test_auth.py
+++ api/v2/tests/test_auth.py
@@ -41,6 +41,14 @@ class AuthTestCase(unittest.TestCase):
 		self.assertEqual(rev.status_code, 202)
 
 
+	def tests_invalid_data(self):
+		""" tests invalid data in not allowed """
+
+		users_data = {'username':'#$@#emo', 'email': 'testtest.com', 'password': '@##st1023'}
+		reg = self.app.post('/api/v2/auth/register', data = json.dumps(users_data), content_type = 'application/json')
+		self.assertEqual(reg.status_code, 403)
+
+
 	def test_user_login(self):
 		""" tests a user can login """
 
@@ -51,21 +59,38 @@ class AuthTestCase(unittest.TestCase):
 
 
 	def tests_non_registered_user_login(self):
-		""" test non registered users can be logged in """
+		""" test non registered users cant be logged in """
 
 		users_data = {'email': 'test@test.com', 'password': 'test1023'}
 		reg = self.app.post('/api/v2/auth/login', data = json.dumps(users_data), content_type = 'application/json')
 		self.assertEqual(reg.status_code, 401)  #unauthorized access
 
-	def tests_user_resets_password(self):
-		""" tests a registered user can reset-password """
+
+	def tests_user_logout(self):
+		""" test a user logs out successfully """
+
 		reg = self.app.post('/api/v2/auth/register', data = json.dumps(self.user_data), content_type = 'application/json')
 		self.assertEqual(reg.status_code, 201)
-		users_data = {'email':'demo@test.com', 'new_password': 'test1023'}
-		res = self.app.post('/api/v2/auth/reset-password', data = json.dumps(users_data), content_type = 'application/json')
+		rev = self.app.post('/api/v2/auth/login', data = json.dumps(self.user_data), content_type = 'application/json')
+		self.assertEqual(rev.status_code, 200)
+		token = json.loads(rev.data.decode())['token']
+		result = self.app.post('/api/v2/auth/logout', headers = {'Content-Type':'application/json','Authorization':token})
+		self.assertEqual(result.status_code, 200)
+		
+
+	def test_user_reset_password(self):
+		""" tests user successfully reset their passsword """
+
+		reg = self.app.post('/api/v2/auth/register', data = json.dumps(self.user_data), content_type = 'application/json')
+		self.assertEqual(reg.status_code, 201)
+		rev = self.app.post('/api/v2/auth/login', data = json.dumps(self.user_data), content_type = 'application/json')
+		self.assertEqual(rev.status_code, 200)
+		token = json.loads(rev.data.decode())['token']
+
+		new_data = {'email': 'demo@test.com', 'new_password': 'tests1233'}
+		res = self.app.post('/api/v2/auth/reset-password', data = json.dumps(new_data), headers = {'Content-Type':'application/json',
+			                                                                                       'Authorization':token})
 		self.assertEqual(res.status_code, 200)
-
-
 
 
 
