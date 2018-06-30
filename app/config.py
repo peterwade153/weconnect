3dd1e2ada5d0edc24ddca2a3699963ef1f07a8ed
peterwade153 <peterwade153@gmail.com>
2018-06-30 17:47:30 +0300

    runtime.txt

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config(object):
 	TESTING = False
 	CSRF_ENABLED = True
 	SECRET_KEY = os.getenv('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@localhost/weconnect"
 
 class ProductionConfig(Config):
 	DEBUG = False
